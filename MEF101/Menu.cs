ecd7cdc86031f3babfacf9cdf0819b9d6f92b08f
Bruno Sonnino <sonnino@revolution.com.br>
2016-11-25 12:46:37 -0200

    New project for MEF Introduction

---
+++ MEF101/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEF101
+{
+    public class Menu
+    {
+        [Import]
+        private Module _module;
+
+        public void OptionList()
+        {
+            Console.WriteLine(_module.Title);
+        }
+    }
+
+    [Export]
+    public class Module
+    {
+        public Module()
+        {
+            Title = "Customers";
+        }
+        public string Title { get; set; }
+    }
+}
---
+++ MEF101/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+
+namespace MEF101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            var container = new CompositionContainer(catalog);
+            var menu = new Menu();
+            container.ComposeParts(menu);
+            menu.OptionList();
+            Console.ReadLine();
+        }
+    }
+}
